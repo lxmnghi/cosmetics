c78026e66d9be46bb1190e5f42cc96bec47e89eb
Laxman Ghimire <ilxmn.ghi@gmail.com>
2023-05-23 07:15:25 +0545

    setup a login page

---
+++ client/src/app/login/page.js
@@ -0,0 +1,73 @@
+"use client"
+import Link from 'next/link';
+import '../style/login.css';
+import {useState} from 'react'; 
+
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+     });
+  
+  const {email, password} = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Login successful
+        alert('Login successful');
+
+
+    setFormData({
+      email: '',
+      password: '',
+     
+    });
+
+  } else {
+    //Login failed
+    const errorData = await response.json();
+    alert(errorData.message);
+  }
+} catch (error) {
+  console.log(error);
+  alert('An error occurred during Login');
+}
+
+  };
+
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+    <div className="Login">
+       <input name="email" type="email"  value={email}
+          onChange={handleChange} placeholder="Email" required/><br/>
+      <input name="password"  type="password" value={password}
+          onChange={handleChange} placeholder="Password"  required/><br/>
+
+      <button type="submit" className="Login-button">Login</button>
+      <span className="Login-register-text">
+        Don't have an account yet? <Link href="/register">Register</Link> instead
+      </span>
+    </div>
+    </form>
+  );
+};
+
+export default Login;
---
+++ client/src/app/register/page.js
@@ -0,0 +1,84 @@
+"use client"
+import Link from 'next/link';
+import '../style/register.css';
+import {useState} from 'react';
+
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    
+  });
+  
+  const { fullName, email, password, confirmPassword } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Registration successful
+        alert('Registration successful');
+
+
+    setFormData({
+      fullName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      
+    });
+
+  } else {
+    // Registration failed
+    const errorData = await response.json();
+    alert(errorData.message);
+  }
+} catch (error) {
+  console.log(error);
+  alert('An error occurred during registration');
+}
+
+  };
+
+
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+    <div className="Register">
+      <input name="fullName" type="text"  value={fullName} 
+      onChange={handleChange} placeholder="Full Name" required/><br/>
+      <input name="email" type="email"  value={email}
+          onChange={handleChange} placeholder="Email" required/><br/>
+      <input name="password"  type="password" value={password}
+          onChange={handleChange} placeholder="Password"  required/><br/>
+      <input name="confirmPassword" placeholder="Confirm Password" value={confirmPassword}
+          onChange={handleChange} type="password" required/><br/>
+     
+
+      <button className="Register-button" type="submit">Register</button><br/>
+      <span className="Register-login-text">
+        Already have an account yet? <Link href="/login">Sign in</Link> instead
+      </span>
+    </div>
+    </form>
+  ); 
+};
+
+export default Register;
